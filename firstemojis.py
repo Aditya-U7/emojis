fff1ad8121a884e245923d2d9758dfb5ef930706
Aditya Upadhye <66748266+Aditya-U7@users.noreply.github.com>
2023-06-20 16:56:50 +0530

    Update firstemojis.py

--- firstemojis.py
+++ firstemojis.py
@@ -1,17 +1,22 @@
-''' Printing the modern day emojis of some of the first released emojis. '''
+# Printing the modern day emojis of some of the first released emojis.
 
 import unicodedata
 
-emojis = [0x1F61E, 0x2615, 0x1f30b, 0x1F494, 0x1F4A9, 0x1f427]
+def print_emojis(emojis):
+    
+    print("\nEmoji | Unicode Point | Name\n")
 
-print("\nEmoji | Unicode Point | Name\n")
-
-for emoji in emojis:
+    for emoji in emojis:
       
-    print(chr(emoji).ljust(4), end=" ")
+        print(chr(emoji).ljust(4), end=" ")
     
-    print(f'U+{emoji:05x}'.center(17), end=" ")
+        print(f'U+{emoji:05x}'.center(17), end=" ")
     
-    print(unicodedata.name(chr(emoji)), end="\n\n")
+        print(unicodedata.name(chr(emoji)), end="\n\n")
+
+    
+emojis = [0x1F61E, 0x2615, 0x1f30b, 0x1F494, 0x1F4A9, 0x1f427]     # Unicode code points of emojis in hexadecimal.
+
+print_emojis(emojis)
     
 
